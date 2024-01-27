a5d30c2df58a995e4e3977ee6966e45e5e5358d6
Sahil Kasturi <sahilkasturi2004@gmail.com>
2024-01-27 16:25:23 -0600

    change

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import Sidebar from "./components/sidebar"
 import Header from './components/header';
+import Body from './components/body';
 
 function App() {
   return (
     <div className="App">
       <Sidebar/>
-      <Header/>
+      <div>
+        <Header/>
+        <Body/>
+      </div>
     </div>
   );
 }
 
 export default App;
-
