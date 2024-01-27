0fe6bc498caf4fd51da4bb3d8eb83919ef841278
Sahil Kasturi <sahilkasturi2004@gmail.com>
2024-01-27 15:48:36 -0600

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
 import Sidebar from "./components/sidebar"
-import Body from './components/body';
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
       <Sidebar/>
-      <Body/>
-      <h1>
-          
-      </h1>
+      <Header/>
     </div>
   );
 }
 
 export default App;
+
