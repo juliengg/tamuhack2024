337e82166f3a365d5c68f9c5480e65f11c2bbfec
Sahil Kasturi <sahilkasturi2004@gmail.com>
2024-01-27 16:00:48 -0600

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Sidebar from "./components/sidebar"
 import Header from './components/header';
+import Body from './components/body';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Sidebar/>
       <div>
         <Header/>
+        <Body/>
       </div>
     </div>
   );
