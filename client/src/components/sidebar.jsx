7b47a5f9a04400d02224dfc68b82ad79e520baf0
julien gaucher <julieng.me@gmail.com>
2024-01-27 14:33:33 -0600

    sahil messed up

--- client/src/components/sidebar.jsx
+++ client/src/components/sidebar.jsx
@@ -2,11 +2,24 @@ export default function Sidebar(){
     const sidebarStyle = {
         width:"15vw",
         height:"100vh",
-        background:"red"
+        background: "linear-gradient(#363047, #000000)",
     }
     return(
         <div style={sidebarStyle}>
             <h1>title</h1>
+        </div>
+    )
+}
+
+const buttonStyle = {
+    width:"60%",
+    height:"width"
+}
+
+function Button1(props){
+    return(
+        <div style={buttonStyle}>
+
         </div>
     )
 }
