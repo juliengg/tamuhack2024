e7eb8dad51ccc9f1fa891be8d57e3d5d70629eb4
julien gaucher <julieng.me@gmail.com>
2024-01-27 14:17:08 -0600

    sidebar

--- client/src/components/sidebar.jsx
+++ client/src/components/sidebar.jsx
@@ -1,6 +1,11 @@
 export default function Sidebar(){
+    const sidebarStyle = {
+        width:"15vw",
+        height:"100vh",
+        background:"red"
+    }
     return(
-        <div>
+        <div style={sidebarStyle}>
             <h1>title</h1>
         </div>
     )
