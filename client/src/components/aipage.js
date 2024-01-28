3af554dab1d6bf206f852cc4ea7f774d471eda83
julien gaucher <julieng.me@gmail.com>
2024-01-28 10:33:17 -0600

    jj

--- client/src/components/aipage.js
+++ client/src/components/aipage.js
@@ -1,8 +1,9 @@
 export default function Page3(){
     const pageStyle = {
         margin: "95px 0 0 0",
-
     }
+
+    
     return(
         <div style={pageStyle}>
             <h1>ai stuff</h1>
